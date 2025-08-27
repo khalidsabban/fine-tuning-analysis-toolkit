f9fcb0d1fa50a5550cb243b1b1473efc4f4abf5c
Khalid Sabban <khalidsaban05@gmail.com>
2025-08-27 13:32:40 +0100

    fix code

--- tests/test_model_adapter.py
+++ tests/test_model_adapter.py
@@ -3,7 +3,7 @@ from toolkit.modules.model_adapter import ModelAdapter
 
 def test_model_adapter():
     adapter = ModelAdapter(
-        base_model_name="NousResearch/Llama-2-7b-chat-hf",
+        base_model_name="NousResearch/Llama-2-7b-hf",
         num_labels=2,
         lora_rank=4
     )
